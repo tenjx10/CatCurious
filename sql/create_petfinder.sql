93dcf00e93b70663e4d7701acb97bf695a6871bb
Jay Patel <jaynp110@gmail.com>
2024-11-28 22:26:36 -0500

    this will be the structure of application

---
+++ sql/create_petfinder.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS pets;
+CREATE TABLE pets (
+    """
+    this is the template used in meal_max, we can to figure out the 
+    parameters of petfinder, then we can create the the db table
+    """
+
+
+    """
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    meal TEXT NOT NULL UNIQUE,
+    cuisine TEXT NOT NULL,
+    price REAL NOT NULL,
+    difficulty TEXT CHECK(difficulty IN ('HIGH', 'MED', 'LOW')),
+    battles INTEGER DEFAULT 0,
+    wins INTEGER DEFAULT 0,
+    deleted BOOLEAN DEFAULT FALSE
+    """
+);
