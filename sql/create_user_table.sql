c48ccbb4750542325013816dace04de86aa1719e
irened123 <irenedeng7@gmail.com>
2024-12-10 13:27:58 -0500

    Remove file

--- sql/create_user_table.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    salt TEXT NOT NULL,
-    password TEXT NOT NULL,
-    deleted BOOLEAN DEFAULT FALSE,
-    UNIQUE(artist, title, year)
-);
